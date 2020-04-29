335ca9cc14e6331af6c9dd9f4b0cb2cdccb87f4f
Tom Kwong <tk3369@gmail.com>
2020-04-28 22:44:29 -0700

    updated

--- src/BinaryTraitsTestX.jl
+++ src/BinaryTraitsTestX.jl
@@ -7,4 +7,19 @@ using BinaryTraits
 @trait Fly
 @implement CanFly by liftoff()
 
+struct Swan end
+@assign Swan with Fly
+
+function diagnose()
+    println("__init__: prefix_map = ", BinaryTraits.prefix_map)
+    println("__init__: traits_map = ", BinaryTraits.traits_map)
+end
+
+function testme()
+    diagnose()
+    check(Swan)
+end
+
+__init__() = diagnose()
+
 end # module
